083cd788908825bf851251279454b95758e56c2b
Adell Hanson-Kahn <adell@adell.io>
2015-07-06 00:09:07 -0700

    unsure how to access prototype function from within 67

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -46,11 +46,44 @@ BinarySearchTree.prototype.simpleTree = function(inserts) {
 	}
 };
 
+/*
+balancedTree
+until inserts are only 1
+assign value of current node to middle (1,2,3,4,5) #=> 3
+assign right to return of right-split(4,5) #=> 4
+	right-split:
+	until inserts are only 1
+	assign
+assign left to return of left-split(1,2) #=> 1
+*/
 BinarySearchTree.prototype.balancedTree = function(inserts) {
 	var middleIndex = Math.floor( (inserts.length - 1 ) / 2);
 	var middleInsert = inserts[middleIndex];
-	for (insert in inserts) {
-		this.root.insert(new Node)
+	this.root.insert(new Node(middleInsert));
+	if (inserts.length > 1) {
+		var leftInserts = inserts.slice(0, middleIndex);
+		var rightInserts = inserts.slice(middleIndex + 1, inserts.length);
+
+		this.root.left = new Node( balancedBranch(leftInserts) );
+		this.root.right = new Node( balancedBranch(rightInserts) );
+		
+		return middleInsert;
+	};
+};
+
+BinarySearchTree.prototype.balancedBranch = function(inserts) {
+	if (inserts.length > 1) {
+		var middleIndex = Math.floor( (inserts.length - 1 ) / 2);
+		var middleInsert = inserts[middleIndex];
+		var leftInserts = inserts.slice(0, middleIndex);
+		var rightInserts = inserts.slice(middleIndex + 1, inserts.length);
+
+		this.left.insert(new Node( balancedBranch(leftInserts) ));
+		this.right.insert(new Node( balancedBranch(rightInserts) ));
+		
+		return middleInsert;
+	} else {
+		return inserts[0];
 	}
 };
 // var myNode = new Node(3);
@@ -60,4 +93,5 @@ BinarySearchTree.prototype.balancedTree = function(inserts) {
 bst = new BinarySearchTree(1);
 console.log(bst)
 bst.insert(2)
-bst.simpleTree([4,5,6])
+// bst.simpleTree([4,5,6])
+bst.balancedTree([4,5,6])
